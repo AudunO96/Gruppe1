6ad9b08855d2ce9e925f5df72ce46fac72b6856e
Martin <pedersen.martin9@gmail.com>
2018-02-27 16:58:42 +0100

    Fixed projectile and enemy base

--- Source/Gruppe1/Enemy_base.h
+++ Source/Gruppe1/Enemy_base.h
@@ -16,37 +16,35 @@ public:
 	AEnemy_base();
 
 	UFUNCTION(BlueprintCallable)
-	void recieveHealing(float incomingHealing);
+		float recieveHealing(float incomingHealing);
 
 	UFUNCTION(BlueprintCallable)
-	float getHealth();
+		float getHealth();
 
 	UFUNCTION(BlueprintCallable)
-	float getMaxHealth();
+		float getMaxHealth();
 
 	UFUNCTION(BlueprintCallable)
-	void setHealth();
+		void setHealth();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	//variables
 	UPROPERTY(editAnywhere)
 	float maxHealth;
 
 	UPROPERTY(VisibleAnywhere)
 	float health;
 
-public:	
+	//Gets called when corruption is 0
+	UFUNCTION(BlueprintCallable)
+	void RemoveCorruption();
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	
-	
 };
