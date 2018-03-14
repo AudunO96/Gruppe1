b186f98ee38f91f597a295203bb6c354961ae192
Martin <pedersen.martin9@gmail.com>
2018-03-14 15:00:39 +0100

    Fixed shit

--- Source/Gruppe1/Healing_base.cpp
+++ Source/Gruppe1/Healing_base.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Healing_base.h"
+#include "CharacterBehaviour/PlayerCharacter.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
 
@@ -9,32 +10,34 @@ AHealing_base::AHealing_base()
 {
 	UE_LOG(LogTemp, Warning, TEXT("base constructor"))
 
-	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
-	CollisionComp->InitSphereRadius(CollisionRadius);
-	CollisionComp->SetCollisionProfileName(TEXT("Trigger"));
-	CollisionComp->SetupAttachment(RootComponent);
+	 CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
+	 // CollisionComp->InitSphereRadius(CollisionRadius);
+	 // CollisionComp->SetCollisionProfileName(TEXT("Trigger"));
+	 // CollisionComp->SetupAttachment(RootComponent);
 
-	// Players can't walk on it
-	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
-	CollisionComp->CanCharacterStepUpOn = ECB_No;
+	 //// Players can't walk on it
+	 //CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	 //CollisionComp->CanCharacterStepUpOn = ECB_No;
 
-	// Set as root component
-	RootComponent = CollisionComp;
+	 // Set as root component
+	 RootComponent = CollisionComp;
 
-	// set up overlap
-	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AHealing_base::OnOverlapBegin);
+	 // set up overlap
+	 CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AHealing_base::OnOverlapBegin);
 }
 
 // Called when the game starts or when spawned
 void AHealing_base::BeginPlay()
 {
-	UE_LOG(LogTemp, Warning, TEXT("It's-a me, healing"))
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay base"))
 }
 
 // Called every frame
 void AHealing_base::Tick(float DeltaTime)
 {
-
+	Super::Tick(DeltaTime);
 }
 
 void AHealing_base::deliverHealing(AEnemy_base* target)
@@ -51,15 +54,17 @@ void AHealing_base::OnOverlapBegin(UPrimitiveComponent* OverlappedComp,
 	bool bFromSweep,
 	const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("HIT!"))
+	UE_LOG(LogTemp, Warning, TEXT("%s is hit"), *GetName())
 
 	AEnemy_base* target = Cast<AEnemy_base>(OtherActor);
+
+	APlayerCharacter* isPlayer = Cast<APlayerCharacter>(OtherActor);
 
 	if (target)
 	{
 		deliverHealing(target);
 	}
-	else if(OtherActor != this)
+	else if(OtherActor != this && !isPlayer)
 	{
 		Destroy();
 	}
