1eedf3b6d8302cc0a24db92021f68ead8855005a
Martin <pedersen.martin9@gmail.com>
2018-04-04 13:58:41 +0200

    Introduced death

--- Source/Gruppe1/CharacterBehaviour/PlayerCharacter.cpp
+++ Source/Gruppe1/CharacterBehaviour/PlayerCharacter.cpp
@@ -132,8 +132,15 @@ void APlayerCharacter::SetHealth(float health)
 
 	HealthPercent = mHealth / mMaxHealth;
 
-	if (mHealth <= 1.0f)
+	if (mHealth == 0.0f)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("DEATH GRANTED!"))
 		OnDeath();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("DEATH not GRANTED!"))
+	}
 }
 
 float APlayerCharacter::GetMana()
